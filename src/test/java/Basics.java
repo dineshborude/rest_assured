c6481973a1bd229c2fd4cea9abf57fbe23e92b1c
Dinesh Borude <borudedinesh2@gmail.com>
2026-02-09 00:50:09 +0530

    updated respources

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -1,20 +1,42 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class Basics {
 
     public static void main(String[] args) {
 
-        // validate if add place API is working as expected
-
         RestAssured.baseURI = "https://rahulshettyacademy.com";
+        Response response =
         given()
-                .queryParam("key","qaclick123")
-                .header("Content-Type", "application-json")
-                .body("");
+                .queryParam("key", "qaclick123")
+                .contentType("application/json")
+                .body(Payload.addPlace())
+                .when()
+                .post("/maps/api/place/add/json")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("OK"))
+                .body("scope", equalTo("APP"))
+                .body("place_id", notNullValue())
+                .header("server", "Apache/2.4.52 (Ubuntu)")
+                .extract().response();
 
+        String responseString = response.asString();
+        System.out.println(responseString);
 
+        JsonPath jsResponse = new JsonPath(responseString);
+
+        String placeID = jsResponse.getString("place_id");
+
+        // Update place now
+
+        given().queryParam("key","qaclick123")
+                .contentType("application/json")
+                .body();
 
     }
 }
